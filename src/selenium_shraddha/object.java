a43fea4df7b891584b254a20fe7e5b09762d6e8b
DELL <DELL@DESKTOP-OP2SPUK>
2023-11-30 19:40:02 +0530

    object practice

---
+++ src/selenium_shraddha/object.java
@@ -0,0 +1,149 @@
+package selenium_shraddha;
+
+public class object {
+
+	public static void main(String[] args) {
+	}
+		
+		class smartwatch {
+			
+			 //characteristics = companyname,shape & size, color
+			//Behavior = shows time, heartbets, SO2, foot steps count
+			
+			String  company_name;
+			String  color;
+			double  size;
+			
+			void time (String content) {
+			}	
+		}
+
+			
+			
+			
+			class Fan{
+				
+				 //characteristics = colour, company, number of blades
+				//Behavior = circulates air
+				
+			
+           String company;
+           String colour;
+           int number_of_blades;
+					
+           void circulate_air (String content) {
+			}
+			}
+		
+           
+			
+          class mobile {
+        	//characteristics = brand, ram,screen size
+		    //Behavior = call, msg
+        	  
+        	  String brand_name;
+        	  int ram;
+        	  double screen_size;
+        	  
+        	  void make_call () {
+          
+          }
+          
+		}	
+		
+			
+          
+			class TV  {
+				
+		
+				//characteristics = company, model, screen size;
+				//Behavior = audio. video, channels, ott ;
+				
+				String company;
+				String model;
+				int screensize;
+				
+				void show_channels() {
+				}
+				}
+			
+			
+			
+			class remote {
+			
+			//characteristics = company, size, shape ;
+			//Behavior = access to tv channels, cntrol volume, color settings, google voice search;
+				
+			String company;
+			double size;
+			String shape;
+			
+			void volume_up_down (){
+			}
+			}
+			
+			
+			
+			class Bean_bag {
+			//characteristics = size, shape, material;
+			//Behavior = sitting, relaxing;
+				
+			double size;
+			String shape;
+			String material;
+			
+			void sitting() {
+			}
+			}
+			
+			
+			
+			
+			class dining_table { 
+				
+			//characteristics = size, shape, material;
+			//Behavior = serve meal;
+			
+			double size;
+			String shape;
+			String material;
+			
+			void serve_meal() {
+			}
+			}
+			
+			
+			class door{
+				
+			//characteristics = size, shape, height, width;
+			//Behavior = open & close;
+			
+			int size;
+			String shape;
+			double height;
+			double width;
+			
+			void open_close (){
+			}
+			}
+			
+			class switch_button{ 
+			
+			//characteristics = colour, company, size;
+			//Behavior = on off;
+			
+			String colour;
+			String company;
+			double size;
+			
+			void connect  (){
+			}
+			}
+	}
+            
+	
+		
+			
+
+
+
